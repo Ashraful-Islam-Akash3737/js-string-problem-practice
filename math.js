d74ff34a930897f64b01f15929393bf8e0ef77c1
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-10-27 00:51:01 +0600

    practice about some math operation in math.js file

---
+++ math.js
@@ -0,0 +1,19 @@
+// power()
+console.log(Math.pow(3,5));
+
+
+// round()
+const round = 2.56789;
+console.log(Math.round(round));
+console.log(Math.ceil(round));
+console.log(Math.floor(round));
+
+
+
+// random()
+console.log(Math.random()*20);
+
+console.log(Math.random());
+console.log(Math.random());
+const round120 = Math.random()*50;
+console.log(Math.round(round120));
