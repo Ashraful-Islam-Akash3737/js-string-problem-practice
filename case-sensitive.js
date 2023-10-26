d5eae8b623fa1156eab692d6e795e2a1ed5657d6
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-10-26 16:30:15 +0600

    create case-sensitive.js file to know about case

---
+++ case-sensitive.js
@@ -0,0 +1,9 @@
+const userName = "akash";
+const userInput = "AkasH";
+
+if (userName.toLowerCase() === userInput.toLowerCase()){
+    console.log("valid user name");
+}
+else{
+    console.log("invalid user name");
+}
